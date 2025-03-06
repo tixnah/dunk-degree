4588487e6f5e79fc3f9257443a2a216976a63afa
Nour08Gm <gmatinour16@gmail.com>
2025-03-06 12:15:17 +0100

    by nour

--- main_nour.py
+++ main_nour.py
@@ -1,9 +1,17 @@
 import pygame
 
 pygame.init()
+screen = pygame.display.set_mode((1200, 750))
+clock = pygame.time.Clock()
 running = True
+
 while running:
-    screen = pygame.display.set_mode((1200, 750))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    # fill the screen with a color
+    screen.fill("light blue")
+    pygame.display.flip()
+    #fps
+    clock.tick(60)
+pygame.quit()
