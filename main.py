c71c4094a76fead9952b99f9d419e380aade1696
Audrey-Mallet <audrey.mallet@efrei.net>
2025-04-11 08:31:29 +0200

    audrey : 6th commit 11 mars 2025

---
+++ main.py
@@ -0,0 +1,13 @@
+from test_audrey import menu_event
+from test_audrey import show_img
+
+while running :     # \ main game loop
+    menu_event()   # \ Check for the user input and events
+    show_img()      # \ Draw image on the screen
+    pygame.display.flip()   # \ Update the display
+
+
+pygame.quit()   # \ Quit pygame properly
+
+
+
